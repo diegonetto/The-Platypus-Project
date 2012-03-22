83c39d1139815316685d1baf57928d49622f9a82
Diego Netto <diegormnetto@gmail.com>
2012-03-22 14:51:40 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Platypus::Application.config.secret_token = 'ef76d0cbfc0a675f55ff2a574744028286018581b2e99d53e5e70cc4e758f4e706ba6a1317b48e97ce456b5022fb112cd3c3c6a7cb972bcc14e9c352ed559e7d'
