3f0e02a5e3fe4fd70f6a0472870151a549f55431
Diego Netto <diegormnetto@gmail.com>
2012-03-27 05:04:51 -0400

    Updated HTML for the start page, and made it replace the default RoR landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,7 @@ Platypus::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'start#index', as: 'start'
 
   # See how all your routes lay out with "rake routes"
 
